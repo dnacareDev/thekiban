f1da09e87f6ba36d42d09e8f0714acdd593f015f
yadon079 <zeidgame@gmail.com>
2021-10-15 18:35:55 +0900

    docs : 엑셀 업로드 작업 중
    <description>
    - 엑셀 업로드 작업 중
      - 컨트롤러 작성 중
    <issue-number>

---
+++ src/main/java/com/thekiban/utils/ExcelData.java
@@ -0,0 +1,15 @@
+package com.thekiban.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExcelData {
+
+  private Integer num;
+
+  private String name;
+
+  private String email;
+}
