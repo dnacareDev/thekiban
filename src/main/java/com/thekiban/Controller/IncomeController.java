4ec370d8b4076939de313f4b650f199cbeb5cc62
yadon079 <zeidgame@gmail.com>
2021-10-27 22:19:21 +0900

    feature : 목록 출력 기능
    <description>
    - 목록 출력 기능 작성 중
    <issue-number>

--- src/main/java/com/thekiban/Controller/IncomeController.java
+++ src/main/java/com/thekiban/Controller/IncomeController.java
@@ -4,11 +4,12 @@ import com.thekiban.Entity.Income;
 import com.thekiban.Service.IncomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IncomeController {
@@ -34,5 +35,20 @@ public class IncomeController {
     mv.setViewName("redirect:/income");
 
     return mv;
+  }
+
+  @ResponseBody
+  @RequestMapping("incomeList")
+  public Map<String, Object> incomeList(@RequestParam("income_type") String income_type) {
+    System.out.println(123);
+    Map<String, Object> result = new LinkedHashMap<String, Object>();
+
+    List<Income> income = service.SearchIncomeList(income_type);
+
+    result.put("income", income);
+
+    System.out.println(income);
+
+    return result;
   }
 }
--- src/main/java/com/thekiban/Mapper/IncomeMapper.java
+++ src/main/java/com/thekiban/Mapper/IncomeMapper.java
@@ -2,11 +2,20 @@ package com.thekiban.Mapper;
 
 import com.thekiban.Entity.Income;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface IncomeMapper {
 
   // 도입자원 등록
   int InsertIncome(Income income);
+  
+  // 목록 개수
+  int SelectIncomeListCount(@Param("income_type") String income_type);
+
+  // 도입자원 목록
+  List<Income> SearchIncomeList(@Param("income_type") String income_type);
 
 }
