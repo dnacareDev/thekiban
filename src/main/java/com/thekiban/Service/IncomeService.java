6bd09dff84f80052a635d5b6842ce2365b30c8bf
yadon079 <zeidgame@gmail.com>
2021-10-27 19:00:00 +0900

    feature : 기능 수정
    <description>
    - 기능 수정
    <issue-number>

---
+++ src/main/java/com/thekiban/Service/IncomeService.java
@@ -0,0 +1,5 @@
+package com.thekiban.Service;
+
+public interface IncomeService {
+
+}
---
+++ src/main/java/com/thekiban/ServiceImpl/IncomeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.thekiban.ServiceImpl;
+
+import com.thekiban.Service.IncomeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IncomeServiceImpl implements IncomeService {
+
+}
