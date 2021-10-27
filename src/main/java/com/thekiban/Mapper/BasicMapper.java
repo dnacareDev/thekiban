136e96022d50f2b67c806892f1febfbdfada25a8
keh6305 <keh6305@gmail.com>
2021-10-27 18:58:20 +0900

    원종 등록

--- src/main/java/com/thekiban/Mapper/BasicMapper.java
+++ src/main/java/com/thekiban/Mapper/BasicMapper.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.thekiban.Entity.Basic;
 import com.thekiban.Entity.Detail;
+import com.thekiban.Entity.Standard;
 
 @Mapper
 public interface BasicMapper
 {
 	// 세부정보 조회
 	List<Detail> SelectDetail();
+
+	// 원종 등록
+	int InsertBasic(Basic basic);
+
+	// 원정 상세 정보 등록
+	int InsertStandard(List<Standard> standard);
 }
--- src/main/java/com/thekiban/ServiceImpl/BasicServiceImpl.java
+++ src/main/java/com/thekiban/ServiceImpl/BasicServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.thekiban.Entity.Basic;
 import com.thekiban.Entity.Detail;
+import com.thekiban.Entity.Standard;
 import com.thekiban.Mapper.BasicMapper;
 import com.thekiban.Service.BasicService;
 
@@ -20,5 +22,19 @@ public class BasicServiceImpl implements BasicService
 	public List<Detail> SelectDetail()
 	{
 		return mapper.SelectDetail();
+	}
+
+	// 원종 등록
+	@Override
+	public int InsertBasic(Basic basic)
+	{
+		return mapper.InsertBasic(basic);
+	}
+
+	// 원종 상세 정보 등록
+	@Override
+	public int InsertStandard(List<Standard> standard)
+	{
+		return mapper.InsertStandard(standard);
 	}
 }
