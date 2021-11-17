7050ea8d0f12ca399350a56dc7ce2eb5c6a1b8fe
parkejoungho <alfhdoswlfh1024@gmail.com>
2021-11-17 17:42:00 +0900

    toast grid 제작 완료

---
+++ src/main/resources/templates/resource2/common.js
@@ -0,0 +1,767 @@
+const gridData = [
+    {
+      addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+      _id: '1',
+      targetArea: '1',
+      division: '1',
+      matingNum: '1',
+      seedNum: '1',
+      currSeedAmount: '1',
+      germinationRate: '1',
+      pureRate: '1',
+      note: '1',
+    },
+    {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '1',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '2',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '2',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '2',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },
+      {
+        addFile: '<div class="file_input"><input type="file" id=""><label for=""></label> </div>',
+        _id: '1',
+        targetArea: '2',
+        division: '1',
+        matingNum: '1',
+        seedNum: '1',
+        currSeedAmount: '1',
+        germinationRate: '1',
+        pureRate: '1',
+        note: '1',
+      },  
+      
+
+
+    // {
+    //   name: 'X',
+    //   artist: 'Ed Sheeran',
+    //   release: '2014.06.24',
+    //   type: 'Deluxe',
+    //   genre: 'Pop',
+    //   _attributes: {
+    //     disabled: true // A current row is disabled
+    //   }
+    // },
+    // {
+    //   name: 'Moves Like Jagger',
+    //   release: '2011.08.08',
+    //   artist: 'Maroon5',
+    //   type: 'Single',
+    //   genre: 'Pop,Rock',
+    //   _attributes: {
+    //     checkDisabled: true // A checkbox is disabled only
+    //   }
+    // },
+    // {
+    //   name: 'A Head Full Of Dreams',
+    //   artist: 'Coldplay',
+    //   release: '2015.12.04',
+    //   type: 'Deluxe',
+    //   genre: 'Rock',
+    //   _attributes: {
+    //     checked: true, // A checkbox is already checked while rendering
+    //     className: {
+    //       // Add class name on a row
+    //       row: ['red']
+    //     }
+    //   }
+    // },
+    // {
+    //   name: '19',
+    //   artist: 'Adele',
+    //   release: '2008.01.27',
+    //   type: 'EP',
+    //   genre: 'Pop,R&B',
+    //   _attributes: {
+    //     rowSpan: {
+    //       // Merge rows
+    //       artist: 3,
+    //       genre: 2
+    //     }
+    //   }
+    // },
+    // {
+    //   name: '21',
+    //   artist: 'Adele',
+    //   release: '2011.01.21',
+    //   type: 'Deluxe',
+    //   genre: 'Pop,R&B'
+    // },
+    // {
+    //   name: '25',
+    //   artist: 'Adele',
+    //   release: '2015.11.20',
+    //   type: 'EP',
+    //   genre: 'Pop',
+    //   _attributes: {
+    //     className: {
+    //       // Add class name on each columns
+    //       column: {
+    //         type: ['blue'],
+    //         genre: ['blue']
+    //       }
+    //     }
+    //   }
+    // }
+];
+
+
+const _gridData = [
+{
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+},
+{
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '2',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '1',
+    _currSeedAmount: '1',
+    stock: '2',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+    {
+    _id_2: '1',
+    _seedNum: '3',
+    _currSeedAmount: '1',
+    stock: '1',
+    release: '1',
+    redundancy: '1',
+    manager: '1',
+    consignee: '1',
+    _date: '1',
+    endDate: '1',
+    districtDivision: '1',
+    _targetArea: '1',
+    },
+
+];
+
+  let gridDataLeng = gridData.length;
+  
+  for(let i = 0; i < gridDataLeng; i++) {
+      gridData[i].addFile = '<div class="file_input"><input type="file" id="add_file_'+ i +'"><label for="add_file_'+ i +'"></label> </div>';
+  };
+
+
+
+
+
+var grid, grid2;
+window.onload = function() {
+    grid = new tui.Grid({
+        el: document.getElementById('sampleGrid'),
+        data: gridData,
+        scrollX: false,
+        scrollY: true,
+        bodyHeight: 400,
+        rowHeight: '40px',
+      
+        rowHeaders: ['checkbox'],
+        columns: [
+            {
+              header: '첨부파일',
+              name: 'addFile',
+              align: 'center',
+            },
+            {
+              header: '시교명(ID)',
+              name: '_id',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true,
+            },
+            {
+              header: '목표 지역',
+              name: 'targetArea',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '구분',
+              name: 'division',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '교배번호',
+              name: 'matingNum',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '종자번호(ID)',
+              name: 'seedNum',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '현 종자량(g)',
+              name: 'currSeedAmount',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '기내 발아율(%)',
+              name: 'germinationRate',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '기내 순도율(%)',
+              name: 'pureRate',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '비고',
+              align: 'center',
+              name: 'note'
+            }
+          ],
+          pageOptions: {
+            useClient: true,
+            perPage: 10
+        },
+    });
+
+    grid2 = new tui.Grid({
+        el: document.getElementById('outcomeGrid'),
+        data: _gridData,
+        scrollX: false,
+        scrollY: true,
+        bodyHeight: 400,
+        rowHeight: '40px',
+      
+        rowHeaders: ['checkbox'],
+        columns: [
+            {
+              header: '시교명(ID)',
+              name: '_id_2',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true,
+            },
+            {
+                header: '종자번호(ID)',
+                name: '_seedNum',
+                align: 'center',
+                sortingType: 'desc',
+                sortable: true
+            },
+            {
+                header: '종자 보유량',
+                name: '_currSeedAmount',
+                align: 'center',
+                sortingType: 'desc',
+                sortable: true
+            },
+            {
+              header: '입고량',
+              name: 'stock',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '출고량',
+              name: 'release',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '재고량',
+              name: 'redundancy',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '담당자',
+              name: 'manager',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+              header: '인수자',
+              name: 'consignee',
+              align: 'center',
+              sortingType: 'desc',
+              sortable: true
+            },
+            {
+                header: '날짜',
+                name: '_date',
+                align: 'center',
+                sortingType: 'desc',
+                sortable: true
+            },
+            {
+                header: '시교종료일자',
+                name: 'endDate',
+                align: 'center',
+                sortingType: 'desc',
+                sortable: true
+            },
+            {
+                header: '지역구분',
+                name: 'districtDivision',
+                align: 'center',
+                sortingType: 'desc',
+                sortable: true
+            },
+             {
+                header: '대상 지역',
+                name: '_targetArea',
+                align: 'center',
+                sortingType: 'desc',
+                sortable: true
+            },
+          ],
+          pageOptions: {
+            useClient: true,
+            perPage: 10
+        },
+    });
+    
+
+    // table 탭메뉴
+    const tabTableBtn = document.querySelectorAll('.atbd-button-list > .btn-group > .btn');
+
+    let tabIdx = 0;
+    tabTableBtn.forEach((item, idx) => {
+        let dataId = item.classList.contains('on');
+        if(!dataId) {
+            let _dataId = item.getAttribute('data-table');
+            let _tableId = document.getElementById(_dataId);
+            _tableId.style.opacity = '0';
+            setTimeout(() => {_tableId.style.display = 'none'; _tableId.style.opacity = '1'}, 1);
+        };
+
+        item.addEventListener('click', () => {
+            let dataId = item.getAttribute('data-table');
+            let tableId = document.getElementById(dataId);
+
+            tabTableBtn.forEach(items => {
+                let _dataId2 = items.getAttribute('data-table');
+                let _tableId2 = document.getElementById(_dataId2);
+                
+                _tableId2.style.display = 'none';
+            });
+
+            tableId.style.display = 'block';
+            let tIdx = idx === 1 ? tabIdx = 1 : tabIdx = 0; 
+            
+            return false;
+        });
+        return false;
+    });
+
+
+
+
+    // 보여주는 갯수 select선택시 변경
+    const pageLeng = document.getElementById('limit');
+    
+    pageLeng.addEventListener('change', (e) => {
+        let target = e.target;
+        let select = parseInt(target.options[target.selectedIndex].value);
+        let _tab = tabIdx === 0 ? grid.setPerPage(select) : grid2.setPerPage(select);
+    });
+    
+    // Export 기능과 설정
+    const excelExBtn = document.getElementById('excelFileExport');
+
+    const options = {
+        includeHiddenColumns: true,
+        onlySelected: false,
+        columnNames: ['_id', 'targetArea', 'division', 'matingNum', 'seedNum', 'currSeedAmount', 'germinationRate', 'pureRate', 'note'],
+        fileName: 'oo 관리',
+      };
+
+      const options2 = {
+        includeHiddenColumns: true,
+        onlySelected: false,
+        columnNames: ['_id_2', '_seedNum', '_currSeedAmount', 'stock', 'release', 'redundancy', 'manager', 'consignee', '_date', 'endDate', 'districtDivision', '_targetArea'],
+        fileName: 'oo 관리2',
+      };
+
+    excelExBtn.addEventListener('click', () => {
+        let _tab = tabIdx === 0 ? grid.export('xlsx', options) : grid2.export('xlsx', options2);
+    });
+
+
+
+};
+
+
+
+
