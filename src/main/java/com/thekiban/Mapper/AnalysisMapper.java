f3d29acb62242e8cd72f11bacc3e783314cb209d
keh6305 <keh6305@gmail.com>
2021-11-20 01:19:22 +0900

    통합 분석 수정

--- src/main/java/com/thekiban/Mapper/AnalysisMapper.java
+++ src/main/java/com/thekiban/Mapper/AnalysisMapper.java
@@ -13,10 +13,11 @@ import com.thekiban.Entity.Detail;
 public interface AnalysisMapper
 {
 	// 품종 조회
-	List<Breed> SelectBreed(@Param("total_id") int[] total_id, @Param("type") int type);
+	List<Breed> SelectBreed(@Param("name") String name, @Param("total_id") int[] total_id, @Param("type") int type);
 
 	// 원종 조회
-	List<Basic> SelectBasic(@Param("total_id") int[] total_id, @Param("type") int type);
+	List<Basic> SelectBasic(@Param("name") String name, @Param("total_id") int[] total_id, @Param("type") int type);
 
-	List<Detail> SelectTrait(String deatil_name);
+	// 분석 형질 조회
+	List<Detail> SelectTrait(@Param("detail_name") String deatil_name, @Param("detail_type") int detail_type);
 }
